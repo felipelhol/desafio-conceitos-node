37ad65f6aa4982fb2d367103925aace7c9355639
felipelhol <felipelhol@gmail.com>
2020-05-14 19:26:36 -0300

    Adicionar arquivos atualizados do desafio Node

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,130 @@ app.use(cors());
 
 const repositories = [];
 
+//midleware
+function logRequests(request, response, next){
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+
+  return next();
+}
+function validateRepositoryId(request, response, next){
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid repository ID.'});
+  }
+
+  return next();
+
+}
+
+app.use(logRequests);
+app.use('/repositories/:id', validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  // GET /repositories: Rota que lista todos os repositórios;
+  const { repository } = request.query;
+
+  //console.log(repositories);
+  
+
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  //POST /repositories: A rota deve receber title, url e techs 
+  //dentro do corpo da requisição, sendo a URL o link para o github desse repositório. 
+  //Ao cadastrar um novo projeto, ele deve ser armazenado dentro de um objeto 
+  //no seguinte formato: { id: "uuid", title: 'Desafio Node.js',
+  // url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }; 
+  //Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(), 
+    title, 
+    url,
+    techs,
+    likes: 0
+    };
+    
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+//PUT /repositories/:id: A rota deve alterar apenas  
+//o title,a url e as techs do repositório que possua o id igual ao id presente nos parâmetros da rota;
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryLikes = repositories.find(repository => repository.id === id);
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository with ID not found'});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositoryLikes.likes,
+
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  //DELETE /repositories/:id: A rota deve deletar o repositório com o id presente nos parâmetros da rota;
+  const { id } = request.params;
+ 
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Project not found'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  //POST /repositories/:id/like: 
+  //A rota deve aumentar o número de likes do repositório específico 
+  //escolhido através do id presente nos parâmetros da rota, a cada chamada dessa rota,
+  // o número de likes deve ser aumentado em 1;
+  //let like = 0;
+  const { id } = request.params;
+
+  const repositoryLikes = repositories.find(repository => repository.id === id);
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository with ID not found'});
+  }
+  
+   repositoryLikes.likes++;
+
+   //morelikes =  repositories[repositoryLikes].likes;
+
+  //repositories.push(criarlikes++);
+
+  return response.json(repositoryLikes);
+
+
 });
 
 module.exports = app;
